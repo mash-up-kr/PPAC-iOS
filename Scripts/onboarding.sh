edce4e6d3036665a8236e210b39820fbae11e738
chansooo <kcs05008@gmail.com>
2024-06-03 01:04:21 +0900

    infra: remote 스크립트 변경

--- Scripts/onboarding.sh
+++ Scripts/onboarding.sh
@@ -1,13 +1,7 @@
-if ! command -v brew &> /dev/null; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
 
-
-if ! command -v mise &> /dev/null; then
-  echo "\n[7] > Installing mise ...\n"
-  curl https://mise.run | sh
-fi
-
+curl https://mise.run | sh
+echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
+echo 'export PATH="$HOME/.local/share/mise/shims:$PATH"' >> ~/.zprofile
 # if ! grep -q 'eval "$(~/.local/bin/mise activate zsh)"' ~/.zshrc; then
 #   echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
 # fi
