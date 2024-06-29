00d5ed96db3e04458da920e053d3cd25e7167ad7
hryeong66 <jhr110326@gmail.com>
2024-06-29 15:22:52 +0900

    feat: CircleCopyButton 추가

---
+++ Projects/Core/DesignSystem/Sources/Button/CircleCopyButton.swift
@@ -0,0 +1,27 @@
+//
+//  CircleCopyButton.swift
+//  DesignSystem
+//
+//  Created by 장혜령 on 2024/06/29.
+//
+
+import SwiftUI
+import ResourceKit
+
+public struct CircleCopyButton: View {
+  public var body: some View {
+    Circle()
+      .foregroundStyle(.white)
+      .overlay {
+        ResourceKitAsset.Icon.copy.swiftUIImage
+          .resizable()
+          .scaledToFill()
+          .frame(width: 20, height: 20, alignment: .center)
+      }
+      .frame(width: 42, height: 42, alignment: .center)
+  }
+}
+
+#Preview {
+  CircleCopyButton()
+}
