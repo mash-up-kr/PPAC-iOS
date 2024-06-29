a555fb1475b4e1b3bb63fad60167ff0b18ec40c0
chansooo <89574881+chansooo@users.noreply.github.com>
2024-06-29 21:52:15 +0900

    feat: 데모앱 구현 (#31)

--- Projects/DemoApp/Project.swift
+++ Projects/DemoApp/Project.swift
@@ -1,22 +1,24 @@
 //
 //  Project.swift
-//  Packages
+//  ProjectDescriptionHelpers
 //
-//  Created by kimchansoo on 5/22/24.
+//  Created by kimchansoo on 6/29/24.
 //
+
+import Foundation
 
 import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.configure(
-    name: "App",
+    name: "DemoApp",
     packages: [
     ],
     targets: [
         .configure(
-            name: "Release-Farmeme",
+            name: "DemoApp",
             product: .app,
-            bundleId: "ppac.farmeme.App",
+            bundleId: "ppac.farmeme.DemoApp",
             infoPlist: .extendingDefault(with: [
               "CFBundleShortVersionString": "1.0",
               "CFBundleVersion": "1",
@@ -45,33 +47,6 @@ let project = Project.configure(
                 ]
 //                defaultSettings: .recommended(excluding: [])
             )
-        ),
-        .configure(
-            name: "Develop-Farmeme",
-            product: .app,
-            bundleId: "ppac.farmeme.App",
-            infoPlist: .default,
-            sources: "Sources/**",
-            resources: "Resources/**",
-            dependencies: [
-                .Feature.Home,
-                .Feature.Recommend,
-                .Feature.Search,
-                .Feature.MyPage,
-                .ResourceKit,
-                .Core.DesignSystem,
-                .Core.PPACNetwork,
-                .ThirdParty.Dependency,
-            ],
-            settings: .settings(
-                base: [
-                    "DEVELOPMENT_TEAM": "4NV4Z6BW27",
-                    "CODE_SIGN_STYLE": "Manual",
-                    "PROVISIONING_PROFILE_SPECIFIER": "match Development ppac.farmeme.App",
-                    "CODE_SIGN_IDENTITY": "Apple Development: Chansoo Kim (T7MYKWLF92)"
-                ]
-//                defaultSettings: .recommended(excluding: [])
-            )
-        ),
+        )
     ]
 )
