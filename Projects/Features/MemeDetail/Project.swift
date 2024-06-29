8cd980c27883e39eb0af3bec8d5e1f49bdadcdd0
chansooo <kcs05008@gmail.com>
2024-06-29 13:34:57 +0900

    feat: memeDetail 모듈 추가

---
+++ Projects/Features/MemeDetail/Project.swift
@@ -0,0 +1,30 @@
+//
+//  Project.swift
+//  MemeDetail
+//
+//  Created by kimchansoo on 2024/06/28
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project(
+    name: "MemeDetail",
+    targets: [
+        .configure(
+            name: "MemeDetail",
+            product: .framework,
+            infoPlist: .default,
+            sources: "Sources/**",
+            resources: "Resources/**",
+            dependencies: [
+                .ThirdParty.Dependency,
+                .ThirdParty.Kingfisher,
+                .ResourceKit,
+                .Core.DesignSystem,
+                .Core.PPACModels,
+            ]
+        )
+    ]
+)
+
