c5d8cfdfb70f5cd688fd47c020e33901cce0c4df
hryeong66 <jhr110326@gmail.com>
2024-06-29 17:03:47 +0900

    feat: MemeItemView 추가
    feat: MemeItemView 추가

--- Projects/Core/DesignSystem/Sources/View/MemeItemView.swift
+++ Projects/Core/DesignSystem/Sources/View/MemeItemView.swift
@@ -6,10 +6,26 @@
 //
 
 import SwiftUI
-
+import ResourceKit
 public struct MemeItemView: View {
-  public init() { }
+  //private let imageUrl: String
+  private let memeName: String
+  private let reaction: Int
+  public init(memeName: String, reaction: Int) {
+    self.memeName = memeName
+    self.reaction = reaction
+  }
+  
   public var body: some View {
+    VStack {
+      MemeItemViewWithButton()
+      MemeItemInfoView(memeName: memeName, reaction: reaction)
+    }
+  }
+}
+
+struct MemeItemViewWithButton: View {
+  var body: some View {
     ZStack(alignment: .bottomLeading) {
       VStack {
         Rectangle() // memeImageView가 와야함
@@ -24,6 +40,36 @@ public struct MemeItemView: View {
   }
 }
 
+struct MemeItemInfoView: View {
+  let memeName: String
+  let reaction: Int
+  
+  var body: some View {
+    HStack {
+      VStack(alignment: .leading, spacing: 6) {
+        Text(memeName)
+          .lineLimit(2)
+        if reaction > 0 {
+          memeReactionView
+        }
+        
+      }
+      .padding(EdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 0))
+      Spacer()
+    }
+  }
+  
+  var memeReactionView: some View {
+    HStack {
+      ResourceKitAsset.Icon.ㅋㅋ.swiftUIImage
+        .renderingMode(.template)
+      Text("\(reaction)")
+    }
+    .foregroundStyle(Color.Text.tertiary)
+  }
+}
+
+
 #Preview {
-  MemeItemView()
+  MemeItemView(memeName: "재성이의 소중한 제보 재성이의 소중한 제보 재성이의 소중한 제보 재성이의 소중한 제보 재성이의 소중한 제보 재성이의 소중한 제보 재성이의 소중한 제보 ",reaction: 10)
 }
