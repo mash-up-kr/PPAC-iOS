7201bb5a9c745cd2242de5d4fa289766ab256ebf
chansooo <kcs05008@gmail.com>
2024-07-12 00:16:46 +0900

    feat: 홈, 상세에서 사용하는 meme 관련 usecase 구현

---
+++ Projects/Core/PPACDomain/Sources/Meme/ReactToMemeUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  ReactToMemeUseCase.swift
+//  PPACDomain
+//
+//  Created by kimchansoo on 7/11/24.
+//
+
+import Foundation
+
+import PPACModels
+
+public protocol ReactToMemeUseCase {
+    func execute(memeId: String, deviceId: String) async throws
+}
+
+public class ReactToMemeUseCaseImpl: ReactToMemeUseCase {
+    private let repository: MemeRepository
+
+    public init(repository: MemeRepository) {
+        self.repository = repository
+    }
+
+    public func execute(memeId: String, deviceId: String) async throws {
+        try await repository.reactToMeme(memeId: memeId, deviceId: deviceId)
+    }
+}
