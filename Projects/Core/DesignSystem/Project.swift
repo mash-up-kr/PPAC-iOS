45a5879aee86d5ceda2919eaed51da7e2cb29ee7
jongnan <dirlawhddbs@gmail.com>
2024-07-27 15:24:50 +0900

    feat: 디펜던시 추가

--- Projects/Core/DesignSystem/Project.swift
+++ Projects/Core/DesignSystem/Project.swift
@@ -18,6 +18,7 @@ let project = Project(
             resources: "Resources/**",
             dependencies: [
                 .ResourceKit,
+                .ThirdParty.PopupView,
                 .Core.PPACModels
             ]
         )
