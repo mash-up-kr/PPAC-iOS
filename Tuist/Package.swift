d314dfe0d1bbe34e7675566bc53e3c4d9f438002
hryeong66 <jhr110326@gmail.com>
2024-08-21 02:21:28 +0900

    Etc: develop 내용 합치면서 빠진 부분 수정 (#67)

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.4.3"),
         .package(url: "https://github.com/onevcat/Kingfisher", from: "7.12.0"),
         .package(url: "https://github.com/exyte/PopupView.git", from: "3.0.4"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.27.0")
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.27.0"),
         .package(url: "https://github.com/CSolanaM/SkeletonUI.git", from: "2.0.2")
     ]
 )
